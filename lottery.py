36557bfe63550ead1318aae95d20d914b7579e6a
Kai <malnemark@yahoo.com>
2018-09-02 11:37:00 +0200

    added exception handling for KeyError, Queries are functions now.

--- lottery.py
+++ lottery.py
@@ -1,21 +1,14 @@
-import sys, json, time, logging
+#!/usr/bin/env python3
 
-# TODO the assetregister will be read from a JSON or even from the blockchain itself, it needs to list all available assets.
-assetreg = ['assetid1', 'assetid2','assetid3','assetid4','assetid5','assetid6']
+import sys, json, time, logging, argparse
 
 
-# Storing a passphrase in plain text is dangerous. Only use this on a local node, and maybe rather for
-# fun not for serious amounts of Ardor/Ignis.
-passPhrase = "top secret collection of words"
-BaseAccount = "ARDOR-ACCOUNT-RELATED-TO-THE-PHRASE"
-
-def sendQuery(Query):
+def sendQuery(nodeurl,Query):
   """function to query the Ardor API"""
   from urllib import parse, request
   params = parse.urlencode(Query)
   headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-  # port is the testnet port, change it for mainnet use.
-  req = request.Request("http://localhost:26876/nxt", params.encode('utf-8'))
+  req = request.Request(nodeurl, params.encode('utf-8'))
   response = request.urlopen(req)
   return response.read()
 
@@ -29,12 +22,15 @@ def detectBuy(data, price, acc, msg):
     """
     logger = logging.getLogger('ARDR_lottery')
     buys = []
-    for e in data['transactions']:
-        if e['recipientRS'] == acc:
-            if 'message' in e['attachment'].keys():
-                if e['amountNQT'] == price and e['attachment']['message'] == msg:
-                    logger.info('buy transaction detected from account:',e['senderRS'],' with fullHash: ',e['fullHash'])
-                    buys.append({'buyer':e['senderRS'],'height':e['height'],'confirmations':e['confirmations'],'message':e['attachment']['message'],'fullHash':e['fullHash']})
+    try:
+        for e in data['transactions']:
+            if e['recipientRS'] == acc:
+                if 'message' in e['attachment'].keys():
+                    if e['amountNQT'] == price and e['attachment']['message'] == msg:
+                        logger.info('buy transaction detected from account:',e['senderRS'],' with fullHash: ',e['fullHash'])
+                        buys.append({'buyer':e['senderRS'],'height':e['height'],'confirmations':e['confirmations'],'message':e['attachment']['message'],'fullHash':e['fullHash']})
+    except:
+        return buys
     return buys
 
 
@@ -64,9 +60,9 @@ def matchPurchases(data,buys,acc):
 
 
 
-#class WheelOfFortune(objects)
+
 def spin(array):
-    """the function picking an asset from the list, can pick duplicates if ran multiple times"""
+    """Spin the wheel of fortune - the function picking an asset from the list, note: it can pick duplicates"""
     import random
     num_choices = len(array)
     secure_random = random.SystemRandom()
@@ -75,41 +71,54 @@ def spin(array):
 
 # a fixed query to ask for purchases
 #TODO read out blockTimestamp and set a min timestamp to filter out old TX
-QueryPayments = {'requestType': 'getBlockchainTransactions',
-'account': BaseAccount, 'type': '0', 'subtype': '0', 'executedOnly': 'true','chain':'IGNIS'}
+def QueryPayments(account):
+    return {'requestType': 'getBlockchainTransactions', 'account': account, 'type': '0', 'subtype': '0', 'executedOnly': 'true','chain':'IGNIS'}
 
 
 # a fixed query to ask outgoing transactions
 #TODO read out blockTimestamp and set a min timestamp to filter out old TX
-QueryAssetTransfers = {'requestType': 'getBlockchainTransactions',
-'account': BaseAccount, 'type': '2', 'subtype': '1', 'executedOnly': 'true','chain':'IGNIS'}
+def QueryAssetTransfers(account):
+    return {'requestType': 'getBlockchainTransactions', 'account': account, 'type': '2', 'subtype': '1', 'executedOnly': 'true','chain':'IGNIS'}
 
 # a fixed query to ask unconfirmed outgoing transactions (to prevent double spending)
-QueryUnconfirmedDeliveries = {'requestType': 'getUnconfirmedTransactions',
-'account': BaseAccount,'chain':'IGNIS'}
-
-
+def QueryUnconfirmedDeliveries(account):
+    return {'requestType': 'getUnconfirmedTransactions', 'account': account,'chain':'IGNIS'}
 
 if __name__ == "__main__":
+
+    #set up a simple logger
     logger = logging.getLogger('ARDR_lottery')
     logger.setLevel(logging.INFO)
     consoleHandler = logging.StreamHandler(sys.stdout)
     logger.addHandler(consoleHandler)
+
+    # get arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--account', help='lottery account', required=True)
+    parser.add_argument('--passphrase', help='passphrase of lottery account', required=True)
+    parser.add_argument('--nodeurl', help='nodeurl, default is http://localhost:26876/nxt', default="http://localhost:26876/nxt")
+    args = parser.parse_args()
+
     while True:
+        status = sendQuery(args.nodeurl,{'requestType':'getBlockchainStatus'});
+
         logger.info("querying the blockchain for a new purchase")
-        output = sendQuery(QueryPayments)
+        output = sendQuery(args.nodeurl,QueryPayments(args.account))
         payments = json.loads(output.decode('utf-8'))
 
         buys = detectBuy(payments,"1200000000",BaseAccount,'lottery')
 
-        output = sendQuery(QueryAssetTransfers)
+        output = sendQuery(args.nodeurl,QueryAssetTransfers(args.account))
         assettransfers = json.loads(output.decode('utf-8'))
 
-        output = sendQuery(QueryUnconfirmedDeliveries)
+        output = sendQuery(args.nodeurl,QueryUnconfirmedDeliveries(args.account))
         unconfDeliveries = json.loads(output.decode('utf-8'))
 
-        allDeliveries = assettransfers['transactions'] + unconfDeliveries['unconfirmedTransactions']
-        deliveryRequired = matchPurchases(assettransfers['transactions'],buys,BaseAccount)
+        try:
+            allDeliveries = assettransfers['transactions'] + unconfDeliveries['unconfirmedTransactions']
+            deliveryRequired = matchPurchases(assettransfers['transactions'],buys,BaseAccount)
+        except KeyError:
+            deliveryRequired = None # instead of False..
 
         if deliveryRequired:
             for d in deliveryRequired:
@@ -119,11 +128,11 @@ if __name__ == "__main__":
                 #TODO add a doublespend protection -> set a flag TRUE if a delivery has been transmitted, wait for N cycles until delivery would be repeated
                 for aid in assetId:
                     QueryTransferAsset = {'chain':'IGNIS','requestType': 'transferAsset', 'recipient': d['buyer'], 'asset': aid, 'secretPhrase':passPhrase, 'broadcast':'true','message':d['fullHash'],'messageIsText':'true','quantityQNT':'1','feeNQT':'101000000'}
-                    resp = sendQuery(QueryTransferAsset)
+                    resp = sendQuery(args.nodeurl,QueryTransferAsset)
                     response = json.loads(resp.decode('utf-8'))
                     if 'errorCode' in response.keys():
                         print('response: ',response)
                     else:
                         logger.info('assetTransfer success.')
-        print('pausing')
+        logger.info('pausing')
         time.sleep(50)
